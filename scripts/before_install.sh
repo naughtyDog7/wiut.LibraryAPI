604c391edec50dfa49bcf7b7e0ee13e42bfe3ef0
Elyor Khojikhonov <e.hadjiahnov@gmail.com>
2023-06-05 00:52:40 +0500

    update before install script name

---
+++ scripts/before_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cat <<EOF > /etc/systemd/system/libraryapi.service
+[Unit]
+Description=LibraryAPI Service
+After=network.target
+
+[Service]
+WorkingDirectory=/var/www/LibraryAPI
+ExecStart=/usr/bin/dotnet run
+Restart=always
+RestartSec=10
+SyslogIdentifier=libraryapi
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable libraryapi.service
